e551faad6915b316fa7f7a1156ac09116e8b3e3e
Satoru Kikuchi <str.kikuchi@gmail.com>
2020-10-17 00:20:39 +0900

    Setup for typescript

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+
+const App = ({Component, pageProps}: AppProps): any => {
+  return <Component {...pageProps} />;
+};
+
+export default App
