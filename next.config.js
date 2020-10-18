3eb21ee1f8a6fd83a6654ea933c6c9136de98422
Satoru Kikuchi <str.kikuchi@gmail.com>
2020-10-18 16:31:36 +0900

    Set up directory alias

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const path = require('path')
 
 module.exports = {
   webpack(config, options) {
-    config.resolve.alias['@'] = path.join(__dirname, 'src')
-    return config
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    config.resolve.alias['@@'] = __dirname;
+    return config;
   },
 }
