8f36e8dcf6bd08bacb780010edc243d3adb080b9
Satoru Kikuchi <str.kikuchi@gmail.com>
2020-11-07 05:13:16 +0900

    Add constants

---
+++ src/constants/color.ts
@@ -0,0 +1,1 @@
+export const PRIMARY = '#FF6600';
---
+++ src/constants/mediaquery.ts
@@ -0,0 +1,2 @@
+export const TABLET_MEDIA = '@media screen and (max-width: 860px)';
+export const MOBILE_MEDIA = '@media screen and (max-width: 600px)';
