80753eb218d314ab78d6e6feb39469fe6dfb31aa
Satoru Kikuchi <str.kikuchi@gmail.com>
2021-05-04 15:51:41 +0900

    Delete unnecessary value

--- src/common/hooks/swr/useUser.ts
+++ src/common/hooks/swr/useUser.ts
@@ -2,16 +2,13 @@ import useSWR, { SWRResponse } from 'swr';
 import { fetchUser } from '@/common/api';
 import { User } from '@/common/models';
 
-export function useUser(id?: string): SWRResponse<User | undefined, Error> {
+export function useUser(id?: string): Pick<SWRResponse<User | undefined, Error>, 'data' | 'error'> {
   const key: string | undefined = id ? JSON.stringify(`/api/users/${id}`) : undefined;
   const fetcher = async () => fetchUser(id).then((user: User) => user);
 
-  const { data, mutate, revalidate, error, isValidating } = useSWR(key, fetcher);
+  const { data, error } = useSWR(key, fetcher);
   return {
     data,
-    mutate,
-    revalidate,
     error,
-    isValidating,
   };
 }
