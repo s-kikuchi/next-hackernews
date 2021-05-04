74d124be84d53256bed41266c9b83ec02eb8dd75
Satoru Kikuchi <str.kikuchi@gmail.com>
2021-05-04 15:42:58 +0900

    Add constant max number of showing item in item index page

--- src/common/utils/constants.ts
+++ src/common/utils/constants.ts
@@ -8,3 +8,6 @@ export const MOBILE_WINDOW = { width: 600 };
 // media query
 export const TABLET_MEDIA = `@media screen and (max-width: ${TABLET_WINDOW.width}px)`;
 export const MOBILE_MEDIA = `@media screen and (max-width: ${MOBILE_WINDOW.width}px)`;
+
+// items
+export const ITEMS_PER_PAGE = 20;
